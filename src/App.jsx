6ae29f809fc5157de89b3efcb6c9f20921990026
Nikki7150 <kanchan.nishka@gmail.com>
2026-08-04 00:31:43 -0700

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Explore from './pages/Explore'
 import Login from './pages/Login'
 import EventDetails from './pages/EventDetails'
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/explore" element={<Explore />} />
-      <Route path="/event/:eventId" element={<EventDetails />} />
-    </Routes>
+    <div className="App" style={{ paddingLeft: '70px' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/event/:eventId" element={<EventDetails />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import '../styles/Navbar.css';
+import { NavLink } from 'react-router-dom';
+import { FaSearch, FaRegUser, FaHome, FaRegCheckCircle } from "react-icons/fa";
+import { IoBookmarkOutline } from 'react-icons/io5';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <h1>Encore</h1>
+            </div>
+            <div className="navbar-links">
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <span className="nav-icon"><FaHome size={25} /></span>
+                    <span className="nav-label">Home</span>
+                </NavLink>
+                <NavLink to="/explore" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <span className="nav-icon"><FaSearch size={25} /></span>
+                    <span className="nav-label">Explore</span>
+                </NavLink>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <span className="nav-icon"><IoBookmarkOutline size={25} /></span>
+                    <span className="nav-label">My Events</span>
+                </NavLink>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <span className="nav-icon"><FaRegCheckCircle size={25} /></span>
+                    <span className="nav-label">Following</span>
+                </NavLink>
+            </div>
+            <div className="navbar-profile">
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <span className="nav-icon"><FaRegUser size={25} /></span>
+                    <span className="nav-label">Profile</span>
+                </NavLink>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
