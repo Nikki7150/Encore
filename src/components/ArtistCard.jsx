de6830bcfe50c350842c199f8e2b0625e1e2af4b
Nikki7150 <kanchan.nishka@gmail.com>
2026-08-06 00:22:54 -0700

    following page done

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,14 +1,74 @@
-const ArtistCard = ({ artist, events, isFollowed, onToggleFollow, searchQuery }) => {
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import '../styles/Explore.css';
+import { searchEventsByArtist } from "../api/ticketmaster";
+import LoadingSpinner from './LoadingSpinner';
+
+const ArtistCard = ({ artist, isFollowed, onToggleFollow, searchQuery }) => {
+    const [showEvents, setShowEvents] = useState(false);
+    const [events, setEvents] = useState(null);
+    const [loadingEvents, setLoadingEvents] = useState(false);
+
+    const handleEventsClick = async () => {
+        if (showEvents) {
+            setShowEvents(false);
+            return;
+        }
+        if (events !== null) {
+            setShowEvents(true);
+            return;
+        }
+        setLoadingEvents(true);
+        setShowEvents(true);
+        try {
+            const eventsData = await searchEventsByArtist(artist.name);
+            setEvents(eventsData);
+        } catch (error) {
+            console.error("Error fetching events:", error);
+        } finally {
+            setLoadingEvents(false);
+        }
+    };
+
     return (
         <div className="artist-card">
             <h2 className="artist-name">{artist.name}</h2>
             <img src={artist.images?.[0]?.url} alt={artist.name} className="artist-image" />
-            <button
-                className={`follow-button ${isFollowed ? 'unfollow' : 'follow'}`}
-                onClick={() => onToggleFollow(artist)}
-            >
-                {isFollowed ? 'Unfollow' : 'Follow'}
-            </button>
+            <div className="artist-description">
+                <p className="artist-genre">Genre: {artist.classifications?.[0]?.genre?.name || "Unknown Genre"}</p>
+                <a href={artist?.url} target="_blank" rel="noopener noreferrer" className="artist-info">
+                    More Info
+                </a>
+                <button
+                    className={`${isFollowed ? 'unfollow-button' : 'follow-button'}`}
+                    onClick={() => onToggleFollow(artist)}
+                >
+                    {isFollowed ? 'Unfollow' : 'Follow'}
+                </button>
+                <div className="artist-events-button" onClick={handleEventsClick} >
+                    <p>Events- {showEvents ? 'Hide' : 'Show'}</p>
+                </div>
+            </div>
+            {showEvents && (
+                <div className="artist-events-popup">
+                    {loadingEvents ? (
+                        <LoadingSpinner />
+                    ) : (
+                        <div className="artist-events-list">
+                            <h3>Upcoming Events:</h3>
+                            <ul>
+                                {events?.map((event) => (
+                                    <li key={event.id}>
+                                        <Link to={`/event/${event.id}`} state={{ event, searchQuery }} className="artist-event-link">
+                                            {event.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
